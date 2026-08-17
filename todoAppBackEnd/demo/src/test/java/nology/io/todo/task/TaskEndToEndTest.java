6609a51175c7102b06772ddd1383b8c4fd9509ab
PikMot <kiptom6@gmail.com>
2026-08-17 21:56:22 +1000

    added test for Patch and delete

--- todoAppBackEnd/demo/src/test/java/nology/io/todo/task/TaskEndToEndTest.java
+++ todoAppBackEnd/demo/src/test/java/nology/io/todo/task/TaskEndToEndTest.java
@@ -143,7 +143,7 @@ public class TaskEndToEndTest {
 
     //creating task via DTO/ hash map
     @Test
-    public void createBook_ValidDto_Created(){
+    public void createTask_ValidDto_Created(){
 
          HashMap<String, String> data = new HashMap<>();
          data.put("title", "Title 1");
@@ -159,6 +159,116 @@ public class TaskEndToEndTest {
             .body("status", matchesPattern("START"))
             .body(matchesJsonSchemaInClasspath("schemas/task-schema.json"));
 
+
+    }
+
+    //patch with bad body
+
+    @Test
+    public void patchTask_InvalidBodyDto_BadRequest(){
+
+        //arrange
+
+        //need to have existint task with id
+        Task task1 = new Task();
+        task1.setTitle("Title 1");
+        task1.setDescription("Description 1");
+        task1.setStatus("START");
+
+        taskRepository.saveAndFlush(task1);
+
+        HashMap<String, String> data = new HashMap<>();
+        data.put("title", "");
+
+        given().contentType(ContentType.JSON).body(data)
+                .when().patch("/tasks/" + task1.getId())
+                .then().log().body()
+                .statusCode(HttpStatus.BAD_REQUEST.value()); //caught springs error
+
+    }
+
+    //patch with invalid id
+
+    @Test
+    public void patchTask_InvalidId_NotFound(){
+
+        //has task but can't find ID
+        HashMap<String, String> data = new HashMap<>();
+        data.put("title", "Title 1");
+        data.put("description", "Description 1");
+        data.put("status", "START");
+
+        given().contentType(ContentType.JSON).body(data)
+                .when().patch("tasks/1")
+                .then().log().body()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .body("error", matchesPattern("Not Found"))
+                .body(matchesJsonSchemaInClasspath("schemas/api-error-schema.json"));
+
+    }
+
+    //sucess patch
+
+    @Test
+    public void patchTask_ValidIdBody_Success(){
+
+        HashMap<String, String> data = new HashMap<>();
+        data.put("title", "Title NEW");
+        data.put("description", "Description NEW");
+        data.put("status", "FINISHED");
+
+        Task task1 = new Task();
+        task1.setTitle("Title 1");
+        task1.setDescription("Description 1");
+        task1.setStatus("START");
+
+        taskRepository.saveAndFlush(task1);
+
+        given().contentType(ContentType.JSON).body(data)
+                .when().patch("tasks/" + task1.getId())
+                .then().log().body()
+                .statusCode(HttpStatus.OK.value())
+                .body("title", matchesPattern("Title NEW"))
+                .body("description", matchesPattern("Description NEW"))
+                .body("status", matchesPattern("FINISHED"))
+                .body(matchesJsonSchemaInClasspath("schemas/task-schema.json"));
+
+    }
+    
+    //delete fail
+    @Test
+    public void deleteTask_invalidId_NotFound(){
+
+        //arrange
+        long id = 1L;
+
+        //testing body type
+        given().when().delete("/tasks/" + id)
+                .then().statusCode(HttpStatus.NOT_FOUND.value())
+                .body("message", matchesPattern("CAN'T FIND AND DELETE TASK THAT DOESN'T EXIST with ID " + id))
+                .body("error", matchesPattern("Not Found"))
+                .body(matchesJsonSchemaInClasspath("schemas/api-error-schema.json"));
+
+    }
+
+    //delete success
+    @Test
+    public void deleteTask_validId_Success(){
+
+        //arrange
+        Task task1 = new Task();
+        task1.setTitle("Title 1");
+        task1.setDescription("Description 1");
+        task1.setStatus("START");
+
+        taskRepository.saveAndFlush(task1);
+
+        //need to grab current task ID instead of setting 1 after flush -> sets ID after flush
+        long id = task1.getId();
+
+        //testing body type
+        given().when().delete("/tasks/" + id)
+                .then().statusCode(HttpStatus.NO_CONTENT.value());//no body for our case can't validate against schema
 
     }
 
